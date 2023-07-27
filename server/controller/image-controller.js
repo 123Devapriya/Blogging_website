dc79ebe2c379d86cf74e0fa5791e575e19f94c8a
Deva Priya <priyadeva000@gmail.com>
2023-07-27 15:53:01 +0530

    image error resolved

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -19,7 +19,7 @@ export const uploadImage = (request, response) => {
     if(!request.file) 
         return response.status(404).json("File not found");
     
-    const imageUrl = `${url}/file/${request.file.filename}`;
+    const imageUrl = `${url}file/${request.file.filename}`;
 
     response.status(200).json(imageUrl);    
 }
